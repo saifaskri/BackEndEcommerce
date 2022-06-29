34584851db684d65994d06b8efbc19638edf08b1
saifaskri <78469737+saifaskri@users.noreply.github.com>
2022-06-29 13:33:25 +0200

    Update mailer.php

--- MailPackage/mailer.php
+++ MailPackage/mailer.php
@@ -31,8 +31,8 @@ try {
 $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-$mail->Username   = 'saifxt0000@gmail.com';                 //SMTP username
-$mail->Password   = '49a227bea@t';                          //SMTP password
+$mail->Username   = 'email@gmail.com';                 //SMTP username
+$mail->Password   = '';                          //SMTP password
 $mail->SMTPSecure = 'ssl';                                  //Enable implicit TLS encryption
 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
